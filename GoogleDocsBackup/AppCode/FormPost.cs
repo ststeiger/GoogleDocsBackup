08d23befe207df6f8a22403607c33cabd0814943
ststeiger <quandary82@hailmail.net>
2015-02-12 17:07:43 +0100

    Added Form-Post

---
+++ GoogleDocsBackup/AppCode/FormPost.cs
@@ -0,0 +1,54 @@
+﻿
+namespace GoogleDocsBackup
+{
+
+
+    public class FormPost
+    {
+
+        // http://www.codeproject.com/Tips/143208/How-to-Submit-Google-Docs-Form-by-using-Csharp
+        public static void SubmitGoogleDoc()
+        {
+            //Use WebClient Class to submit a new entry
+            System.Net.WebClient wc = new System.Net.WebClient();
+            var keyval = new System.Collections.Specialized.NameValueCollection();
+            keyval.Add("entry.0.single", "XXXX");
+            keyval.Add("entry.1.single", "XXXX");
+            
+            keyval.Add("submit", "Submit");
+            
+            //Create an event for Submit Complete
+            wc.UploadValuesCompleted += new System.Net.UploadValuesCompletedEventHandler(onUploadValuesCompleted);
+            
+            //Create Additional Headers  
+            wc.Headers.Add("Origin", "https://docs.google.com");
+            wc.Headers.Add("User-Agent", "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/534.10 (KHTML, like Gecko) Chrome/8.0.552.224 Safari/534.10");
+            
+            //Finally Submit the Form:
+            wc.UploadValuesAsync(new System.Uri("https://docs.google.com/formResponse?formkey=XXXXX&ifq") 
+                ,"POST"
+                , keyval, System.Guid.NewGuid().ToString()
+            );
+        }
+
+
+        //Submit Complete
+        public static void onUploadValuesCompleted(object sender, System.Net.UploadValuesCompletedEventArgs e)
+        {
+            MsgBox("Finished");
+        }
+
+
+        public static void MsgBox(object obj)
+        {
+            if (obj != null)
+                System.Windows.Forms.MessageBox.Show(obj.ToString());
+            else
+                System.Windows.Forms.MessageBox.Show("obj is NULL");
+        }
+
+
+    }
+
+
+}
---
+++ GoogleDocsBackup/frmMain.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace GoogleDocsBackup
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSubmitForm = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSubmitForm
+            // 
+            this.btnSubmitForm.Location = new System.Drawing.Point(159, 112);
+            this.btnSubmitForm.Name = "btnSubmitForm";
+            this.btnSubmitForm.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmitForm.TabIndex = 0;
+            this.btnSubmitForm.Text = "Submit";
+            this.btnSubmitForm.UseVisualStyleBackColor = true;
+            this.btnSubmitForm.Click += new System.EventHandler(this.btnSubmitForm_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 261);
+            this.Controls.Add(this.btnSubmitForm);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Submit Google-Docs-Form";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSubmitForm;
+    }
+}
+
